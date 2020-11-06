976bb205e78a2f34ace827e0f0366c7766c61002
Franziska Görler <franziska.goerler@gmail.com>
2020-11-06 19:56:03 +0100

    First commit

---
+++ pyforce/nn/action/__init__.py
@@ -0,0 +1,2 @@
+from .action_mapper import ActionMapper
+from .action_processor import ActionProcessor
---
+++ rl_agent.py
@@ -0,0 +1,31 @@
+from pyforce.env import wrap_openai_gym
+# from pyforce.nn import default_network_components
+from pyforce.agents import TD3Agent
+# import gym
+import torch
+
+import pycking_env2
+# from pyforce import agents
+
+# device="cuda:0" if torch.cuda.is_available() else "cpu"
+# torch.cuda.set_device(0)
+device= "cpu"
+# print(device)
+env=wrap_openai_gym(pycking_env2.App())
+
+agent=TD3Agent(
+    env,
+    save_path="./evals/td3_example",
+    critic_lr=1e-3,
+    actor_lr=1e-3
+).to(device)
+
+# agent=TD3Agent(
+#     env,
+#     save_path="./evals/td3_example",
+#     critic_lr=1e-3,
+#     actor_lr=1e-3
+# ).to(device)
+
+# agent.train(env,100000,train_freq=1,batch_size=100,policy_noise=0.1,policy_noise_clip=.25,gamma=.99, policy_freq=2, tau=0.005,warmup_steps=10000,buffer_size=50000, exp_noise=.1,eval_freq=1, render=True, eval_episodes=1)
+agent.train(env,100000,train_freq=1,batch_size=100,policy_noise=0.1,policy_noise_clip=.25,gamma=.99, policy_freq=2, tau=0.005,warmup_steps=10000,buffer_size=50000, exp_noise=.1,eval_freq=1, render=True, eval_episodes=1)
